2863733553f34700983fc1a7150a948db821bf95
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-01-09 23:00:16 +0600

    findtherunnerupscore:fire:

---
+++ FindtheRunnerUpScore.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+   
+    scores = list(set(arr))
+
+    
+    scores.sort(reverse=True)
+
+    runner_up_score = scores[1]
+
+    print(runner_up_score)
