9c786c40944018e16c67149e7689c0ba3afd4592
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-01-11 23:11:59 +0600

    finding the Percentage :books:

---
+++ Findingthepercentage.py
@@ -0,0 +1,15 @@
+# Finding the percentage
+
+if __name__ == '__main__':
+    n = int(input())  
+    student_record = {}  
+
+
+    for _ in range(n):
+        line = input().split()
+        name, marks = line[0], list(map(float, line[1:]))
+        student_record[name] = marks
+    query_name = input()  
+    average_score = sum(student_record[query_name]) / len(student_record[query_name])
+    print("{:.2f}".format(average_score))
+
--- FindtheRunnerUpScore.py
+++ FindtheRunnerUpScore.py
@@ -1,13 +1,16 @@
+# Find The Runner Up Score : 
 if __name__ == '__main__':
+    
     n = int(input())
-    arr = map(int, input().split())
-
-   
-    scores = list(set(arr))
-
+    
+    arr = map(int, input().split())  
+    
+    
+    scores = list(set(arr))   
+    
     
     scores.sort(reverse=True)
-
+    
     runner_up_score = scores[1]
-
+    
     print(runner_up_score)
