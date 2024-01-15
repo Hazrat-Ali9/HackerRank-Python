0f3e2c73a2bfebeca47df282d0e397661691a582
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-01-15 20:52:24 +0600

    NestedLink :books:

---
+++ NestedLinks.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input().strip())
+    students = []
+    for _ in range(n):
+        name = input().strip()
+        score = float(input().strip())
+        students.append([name, score])
+
+    lowest_grade = sorted(set([score for name, score in students]))[1]
+    names_with_second_lowest_grade = sorted([name for name, score in students if score == lowest_grade])  
+
+    for name in names_with_second_lowest_grade:
+        print(name)
