2af5368e84191831e4325294b6f554c9db1a00ea
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-01-17 23:20:55 +0600

    TimeDelta :fire:

---
+++ TimeDelta.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+
+def time_delta(t1, t2):
+    
+    format_str = "%a %d %b %Y %H:%M:%S %z"
+    time1 = datetime.strptime(t1, format_str)
+    time2 = datetime.strptime(t2, format_str)
+
+    
+    time_difference = abs(int((time1 - time2).total_seconds()))
+
+    return time_difference
+
+if __name__ == "__main__":
+    
+    t = int(input().strip())
+
+    for _ in range(t):
+        
+        timestamp1 = input().strip()
+        timestamp2 = input().strip()
+
+        
+        result = time_delta(timestamp1, timestamp2)
+        print(result)
